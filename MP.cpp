cf5ff33446fdcc4bde7283c073ab719859118bfc
Maros <petro.maros.afs@gmail.com>
2021-01-29 10:08:35 +0100

    Draw Histograms working. Need to fix the path and loop.

--- MP.cpp
+++ MP.cpp
@@ -18,7 +18,7 @@ char* str_to_char(string _str)
 {
 	int n = _str.length();
 	char* _char = new char[n+1];
-	
+
     strcpy(_char, _str.c_str());
 
 	return _char;
@@ -90,103 +90,111 @@ void MP()
 	{
 		vector<string>* folders;
 		folders = ReadFiles(p+"/"+years->at(j), "cpg");
-		cout << " =================Year: " << years->at(j) <<"=============" << endl;
-		cout << endl;
+		// cout << " =================Year: " << years->at(j) <<"=============" << endl;
+		// cout << endl;
 
 		for(int i = 0; i < folders->size();i++) //files.size()
 		{
-			cout << " =================folder: " << folders->at(i) << "=============" << endl;
-			cout << endl;
+			// cout << " =================folder: " << folders->at(i) << "=============" << endl;
+			// cout << endl;
 
 			vector<string>* files;
 			files = ReadFiles(p + "/" + years->at(j) + "/" + folders->at(i) , ".root");
 
 			for (int k = 0; k < files->size(); k++)
 			{
-				cout << "File names in folders: " << files->at(k) << endl;
+				// cout << "File names in folders: " << files->at(k) << endl;
 
 				
 				r->year 	= years->at(j);
 				r->folder 	= folders->at(i);
 				r->file 	= files->at(k);
 
-				cout << "r->year  " << r->year << endl;
+				// cout << "r->year  " << r->year << endl;
 
 				root_file_path.push_back(*r);
-				cout<< " root_file_path files" << root_file_path.at(k).year << endl;
+				// cout<< " root_file_path files" << root_file_path.at(k).file << endl;
 			}
 		}
 	}
 	
+	string* s = new string();
+
+	TH1F* h = new TH1F("hh", "haha", 400, 0.0, 4000);
+
+	for(int i = 0; i < 2; i++)
+	{
+		char* root_file;
+
+		// s = &p + "/" + root_file_path.at(0).year + "/" + &root_file_path.at(0).folder + "/" + root_file_path.at(i).file ; 
+
+		cout<< " path :" << p + "/" + root_file_path.at(0).year + "/" + root_file_path.at(0).folder + "/" + root_file_path.at(i).file  << endl;
+
+		root_file = str_to_char(p + "/" + root_file_path.at(0).year + "/" + root_file_path.at(0).folder + "/" + root_file_path.at(i).file );
+		cout<< " c_path :" << root_file << endl;
+
+		TFile* f = new TFile(root_file);
+		TTree* t = (TTree*) f->Get("merged_cal");
 
 
-	// for(int i = 0; i < years->size();i++) //files.size()
-	// {
-	// 	cout << "File names in files: " << years->at(i) << endl;
-	// }
+		vector<double>* e	= new vector<double>();
+		vector<double>* ztc	= new vector<double>();
+		vector<double>* aoe	= new vector<double>();
+		vector<bool>*	fip	= new vector<bool>();
+		vector<bool>*	fbp	= new vector<bool>();
 
-	// char* 	root_file_path;
-	// string 	s; 
+		cout<< "AAAA" << endl;
 
-	// TH1F* h = new TH1F("hh", "haha", 400, 0.0, 4000);
 
-	// for(int i = 0; i < files.size(); i++)
-	// {
-	// 	s = files.at(i);
-	// 	root_file_path = str_to_char(s);
+		t->SetBranchAddress("cal_edep", &e);
+		t->SetBranchAddress("flag_injected_pulse", &fip);
+		t->SetBranchAddress("flag_bad_pulse", &fbp);
+		t->SetBranchAddress("cal_ipos_ztc", &ztc);
+		t->SetBranchAddress("cal_cpg_diff_AoE", &aoe);
 
-	// 	TFile* f = new TFile(root_file_path);
-	// 	TTree* t = (TTree*) f->Get("merged_cal");
+		cout<< "BBB" << endl;
 
-	// 	vector<double>* e	= new vector<double>();
-	// 	vector<double>* ztc	= new vector<double>();
-	// 	vector<double>* aoe	= new vector<double>();
-	// 	vector<bool>*	fip	= new vector<bool>();
-	// 	vector<bool>*	fbp	= new vector<bool>();
 
-	// 	t->SetBranchAddress("cal_edep", &e);
-	// 	t->SetBranchAddress("flag_injected_pulse", &fip);
-	// 	t->SetBranchAddress("flag_bad_pulse", &fbp);
-	// 	t->SetBranchAddress("cal_ipos_ztc", &ztc);
-	// 	t->SetBranchAddress("cal_cpg_diff_AoE", &aoe);
+		int n1 	= t->GetBranch("flag_bad_pulse")->GetEntries();
+		int n2 	= t->GetBranch("flag_injected_pulse")->GetEntries();
+		int n3 	= t->GetBranch("cal_ipos_ztc")->GetEntries();
+		int n4 	= t->GetBranch("cal_cpg_diff_AoE")->GetEntries();
 
-	// 	int n1 	= t->GetBranch("flag_bad_pulse")->GetEntries();
-	// 	int n2 	= t->GetBranch("flag_injected_pulse")->GetEntries();
-	// 	int n3 	= t->GetBranch("cal_ipos_ztc")->GetEntries();
-	// 	int n4 	= t->GetBranch("cal_cpg_diff_AoE")->GetEntries();
+		cout<< "CCC" << endl;
 
-	// 	if(!(n1 == n2 && n2 == n3 && n3 == n4)) // Check if the sizes of leaves are the same. The cut function iterates through size of vector, so they must be same. 
-	// 	{
-	// 		cout << "ERROR: The leaves in this tree are not of the same size!" << endl;
-	// 		cout << "Size of flag_bad_pulse = " 		<< n1				   << endl;
-	// 		cout << "Size of flag_injected_pulse = " 	<< n2				   << endl; 
-	// 		cout << "Size of cal_ipos_ztc = " 			<< n3				   << endl; 
-	// 		cout << "Size of cal_cpg_diff_AoE = " 		<< n4				   << endl; 
 
-	// 		return;
-	// 	}
+		if(!(n1 == n2 && n2 == n3 && n3 == n4)) // Check if the sizes of leaves are the same. The cut function iterates through size of vector, so they must be same. 
+		{
+			cout << "ERROR: The leaves in this tree are not of the same size!" << endl;
+			cout << "Size of flag_bad_pulse = " 		<< n1				   << endl;
+			cout << "Size of flag_injected_pulse = " 	<< n2				   << endl; 
+			cout << "Size of cal_ipos_ztc = " 			<< n3				   << endl; 
+			cout << "Size of cal_cpg_diff_AoE = " 		<< n4				   << endl; 
 
-	// 	for(int i = 0; i < n1 ; i++)
-	// 	{
-	// 		t->GetEntry(i);
+			return;
+		}
 
-	// 		for(int j = 0; j < ztc->size(); j++)
-	// 		{
-	// 			if(   													//apply cuts
-	// 				 !(fip->at(j)) 									&&
-	// 				 !(fbp->at(j)) 									&&
-	// 				  (ztc->at(j) > 0.2 	&& ztc->at(j) < 0.95) 	&&
-	// 				  (aoe->at(j) > 0.872 	&& aoe->at(j) < 1.2 )
-	// 			   )
-	// 			{
-	// 				h->Fill(e->at(j));
-	// 			}
+		for(int i = 0; i < n1 ; i++)
+		{
+			t->GetEntry(i);
 
-	// 		}
+			for(int j = 0; j < ztc->size(); j++)
+			{
+				if(   													//apply cuts
+					 !(fip->at(j)) 									&&
+					 !(fbp->at(j)) 									&&
+					  (ztc->at(j) > 0.2 	&& ztc->at(j) < 0.95) 	&&
+					  (aoe->at(j) > 0.872 	&& aoe->at(j) < 1.2 )
+				   )
+				{
+					h->Fill(e->at(j));
+				}
+
+			}
 			
-	// 	}
-	// }
+		}
+	}
 
-	// h->Draw();
+	h->Draw();
 
 }
